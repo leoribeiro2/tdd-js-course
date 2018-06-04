d27f7ce3082bf47f6a3c48b8f9142c6998bbef57
Leonardo Ribeiro <contato@leoribeiroweb.com.br>
2018-06-04 14:51:20 -0300

    add auth

--- src/config.js
+++ src/config.js
@@ -1,3 +1,11 @@
 export const API_URL = 'https://api.spotify.com/v1/';
 
+const API_TOKEN = 'BQBBKX8uT1OldAC_NeSalUPUfSnSjFYG47FoTCIeq4S';
+
+export const HEADERS = {
+  headers: {
+    Authorization: `Bearer ${API_TOKEN}`,
+  },
+};
+
 export default {};
